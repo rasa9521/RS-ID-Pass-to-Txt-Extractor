c58e7be3618553531d42f966e61402abbc19b022
rasa9521 <rsrasasingh@gmail.com>
2025-03-20 09:43:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ bot_token = "7906984827:AAHK9VXO1oDKCD3f-HfJlL8xnWF4wuE0wiM"
 auth_users = "2093417522"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "2093417522"
-owner_users = [int(num) for num in osowner_users.split("2093417522")]
+owner_users = [int(num) for num in osowner_users.split(",")]
